e40294f72c4674ef9b05ddb96019c23c9693c28f
CarlosAlmendarez <b.almendarezcarlos@gmail.com>
2016-10-14 21:59:06 -0500

    subida inicial
    se crea projecto

---
+++ Prueba local/Space.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Space extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public Space()
+    {    
+        super(1100, 600, 1);
+        Canjuro c = new Canjuro();
+        /*addObject();*/
+    }
+    
+}
